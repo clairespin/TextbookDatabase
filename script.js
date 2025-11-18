48faa830eddf6c6f88cb0d8a41286349d60715c7
clairespin01 <claire.spindler@stu.evsck12.com>
2025-11-18 11:00:15 -0500

    button changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+console.log("JS is connected")
 document.getElementById("show-request").addEventListener("click", function () {
     const container = document.querySelector(".request-container");
     container.style.display = "flex";
+});
+document.getElementById("hide-request").addEventListener("click", function () {
+    const container = document.querySelector(".request-container");
+    container.style.display = "none";
 });
