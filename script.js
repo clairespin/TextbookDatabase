6909ccf7ebcde80489acb4379df94ea9710e7101
clairespin01 <claire.spindler@stu.evsck12.com>
2025-12-04 10:34:09 -0500

    final updates

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 console.log("JS is connected")
-document.getElementById("show-request").addEventListener("click", function () {
-    const container = document.querySelector(".request-container");
-    container.style.display = "flex";
+document.querySelectorAll(".show-request").forEach(button => {
+    button.addEventListener("click", function () {
+        const container = document.querySelector(".request-container");
+        container.style.display = "flex";
+    });
 });
-document.getElementById("hide-request").addEventListener("click", function () {
-    const container = document.querySelector(".request-container");
-    container.style.display = "none";
+
+document.querySelectorAll(".hide-request").forEach(button => {
+    button.addEventListener("click", function () {
+        const container = document.querySelector(".request-container");
+        container.style.display = "none";
+    });
 });
+
+document.querySelectorAll(".hide-request").forEach(button => {
+    button.addEventListener("click", function () {
+        const container = document.querySelector(".request-container");
+        container.style.display = "none";
+    });
+});
+
+const Classes = ["SCLA","CNIT"];
+const Textbooks = []
